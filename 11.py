346bbf2be2bedec81304180c57012ff3254ce110
Pawanpandey7 <pawanpandey2058@gmail.com>
2025-05-15 19:01:01 +0545

    largest number in the list

---
+++ 11.py
@@ -0,0 +1,16 @@
+# to find the largest element in the list 
+
+list1 = [1,5,6,2]
+
+large = list1[0]
+
+for num in list1:
+    if num>large:
+        large = num
+
+    
+print("the largest number in the list is ",large)           
+
+
+
+    
