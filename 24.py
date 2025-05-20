0491128ac5c9f56c6660e3dc235d48eb2e691f22
Pawanpandey7 <pawanpandey2058@gmail.com>
2025-05-20 13:16:00 +0545

    revised selection sort

--- 24.py
+++ 24.py
@@ -4,17 +4,11 @@
 
 def selection_sort(arr):
     n = len(arr)
-    current = arr[0]
-    for i in range(0,n-1):
-        for j in range(i+1,n):
-            if arr[i]>arr[j]:
-                temp = arr[i]
-                arr[i] = arr[j]
-                arr[j] = temp
-
-arr = [10,6,8,2,9,4]
-selection_sort(arr)
-print(arr)
-
+    for i in range(n):
+        min_index = i
+        for j in range(i + 1, n):
+            if arr[j] < arr[min_index]:
+                min_index = j
+        arr[i], arr[min_index] = arr[min_index], arr[i]
 
                 
